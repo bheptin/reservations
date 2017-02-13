c25cc3b488563b8f0cdc735bdb0d9b0737a39a11
Brian H <bheptin@gmail.com>
2017-02-13 16:48:49 -0500

    fix axios call

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-const citySearch = document.querySelector('#cities');
-const dataList = document.querySelector('#textBox');
+const citySearch = document.querySelector('#textBox'); //quesry for actual text
+const dataList = document.querySelector('#cities'); //query for datalist section. Where to put them
 
+
+//Listening for key event then, mapping, turning info over to Axios for call
 citySearch.addEventListener('keyup', function(e) {
   const search = e.target.value;
-  console.log(search);
-  getFilteredCities(search).then(cities => {
-		const suggestions = cities.map(city => `<dataList value="${city}">`).join("\n");
+  getCities(search).then(cities => {
+		const suggestions = cities.map(city => `<option value="${city}">`).join("\n");
 		dataList.innerHTML = suggestions;
 	})
 });
 
-const getFilteredCities = (search) => {
+//Axios call getting info from Teleport. Tried basic Fetch method, but wasn't able
+//to get back info in logical return. Spread operator didn't play well.
+const getCities = (search) => {
 	return axios.get(`https://api.teleport.org/api/cities/?search=${search}`).then(response => {
     return response.data._embedded['city:search-results'].map(city => city.matching_full_name);
-    console.log(response.data);
 	})
-}
+};
