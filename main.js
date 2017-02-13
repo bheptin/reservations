46c802d665deeca9fa4ce16f150c9c790e003f11
Brian H <bheptin@gmail.com>
2017-02-13 14:37:50 -0500

    delete

--- main.js
+++ main.js
@@ -3,15 +3,6 @@ const dataList = document.querySelector('#cityList');
 const api = `https://api.teleport.org/api/cities/?search=${citySearch}`;
 
 
-
-citySearch.addEventListener('keyup', function(e) {
-      const search = e.target.value;
-      pullCities(citySearch).then(cities => {
-        const filteredCity = cityList.map(city => `<option value="${cityList}">`).join("\n");
-        dataList.innerHTML = suggestions;
-      })
-});
-
 const cityState = [];
 
 fetch(api)
