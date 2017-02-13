9c057a2c35ba1774ca8d069f735e0d33f8b6a820
Brian H <bheptin@gmail.com>
2017-02-13 13:20:54 -0500

    add JS

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-$(function($){
-  $.ajax({
-      url: 'https://api.teleport.org/api/cities/?search=${#search}',
-      type: 'GET',
-      data: "city: search-results",
-      success: function(data) {
-        console.log(data);
-      }
-  });
+const citySearch = document.querySelector('#cities');
+const api = `https://api.teleport.org/api/cities/?search={city:search-by-text}`;
 
-})
+
+
+citySearch.addEventListener('keyup', function(e) {
+      const search = e.target.value;
+      getFilteredCities(search).then(cities => {
+        const suggestions = citySearch.map(city => `<option value="${city}">`).join("\n");
+        data.innerHTML = suggestions;
+      })
+});
+
+const cityState = [];
+
+fetch(api)
+  .then(blob => blob.json())
+  .then(curies => cityState.push(console.log(curies)))
+
+function findMatch(citySearch, cityState) {
+  return citySearch.filter(place => {
+
+  });
+}
+
+findMatch('tus');
