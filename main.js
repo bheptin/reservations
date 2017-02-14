5844591f177bb5268ba8a99c33e961703c69a8e2
Brian H <bheptin@gmail.com>
2017-02-13 23:02:21 -0500

    date logged

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const citySearch = document.querySelector('#textBox'); //quesry for actual text
+const citySearch = document.querySelector('#textBox'); //query for actual text
 const dataList = document.querySelector('#cities'); //query for datalist section. Where to put them
 
 
@@ -18,3 +18,45 @@ const getCities = (search) => {
     return response.data._embedded['city:search-results'].map(city => city.matching_full_name);
 	})
 };
+
+
+const formatDate = (dateObj) => {
+	let day = dateObj.getDate();
+	day = day > 9 ? day : `0${day}`;
+	let month = dateObj.getMonth() + 1;
+	month = month.length > 9 ? month : `0${month}`;
+	let year = dateObj.getFullYear();
+	return `${year}-${month}-${day}`;
+}
+
+const getToday = () => {
+	return new Date();
+}
+
+const getTomorrow = () => {
+	let today = new Date();
+	let tomorrow = new Date();
+	tomorrow.setDate(today.getDate() + 1);
+	return tomorrow;
+}
+
+
+const checkIn = document.querySelector('.chkin');
+const checkOut = document.querySelector('.chkout');
+
+checkIn.addEventListener('click', function(d) {
+  const newDate = d.target.value;
+	console.log(newDate);
+});
+
+
+const today = getToday();
+const todayFormat = formatDate(today);
+const tomorrow = getTomorrow();
+const tomorrowFormat = formatDate(tomorrow);
+
+checkIn.setAttribute('min', todayFormat);
+checkIn.setAttribute('value', todayFormat);
+
+checkOut.setAttribute('min', tomorrowFormat);
+checkOut.setAttribute('min', tomorrowFormat);
